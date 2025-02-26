9a2da82b70867528c1f70ec62b98fce031e3e6c2
bfenty <bfenty@gmail.com>
2025-02-25 21:55:14 -0600

    cleaned main

--- main.go
+++ main.go
@@ -1,15 +1,3 @@
-//	@title			Purchasing System API
-//	@version		1.0
-//	@description	API documentation for the Purchasing System
-//	@host			127.0.0.1:8082
-//	@BasePath		/api/
-//	@schemes		http https
-
-// Security Definitions
-//	@securityDefinitions.apikey	ApiKeyAuth
-//	@in							header
-//	@name						X-API-Key
-
 package main
 
 import (
@@ -25,94 +13,75 @@ import (
 	"purchasing/sql"
 	"strings"
 
-	// Swagger files
-	httpSwagger "github.com/swaggo/http-swagger" // HTTP handler for Swagger UI
-
 	"github.com/sirupsen/logrus"
-	log "github.com/sirupsen/logrus"
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
-// initialize Logs
+// Logger instance
 var Logger = logrus.New()
 
-// Main function
 func main() {
-	if os.Getenv("LOGLEVEL") == "DEBUG" {
-		log.SetLevel(log.DebugLevel)
-	} else {
-		log.SetLevel(log.InfoLevel)
-	}
-	log.Info("Starting Server")
+	setupLogging()
+	logrus.Info("Starting Server")
+
 	var message models.Message
 	config.DB, message = config.Opendb()
-	log.Info(message.Body)
+	logrus.Info(message.Body)
 
-	// Serve Swagger UI
-	http.Handle("/swagger/", http.StripPrefix("/swagger/", httpSwagger.Handler(
-		httpSwagger.URL("http://127.0.0.1:8082/docs/swagger.json"), // Swagger endpoint
-	)))
-	fs := http.FileServer(http.Dir("./docs"))
-	http.Handle("/docs/", http.StripPrefix("/docs/", fs))
-	http.HandleFunc("/", PageHandler)
-	http.ListenAndServe(":8082", nil)
+	setupRoutes()
+
+	logrus.Fatal(http.ListenAndServe(":8082", nil))
 }
 
-// Function to check if a file exists
+func setupLogging() {
+	if os.Getenv("LOGLEVEL") == "DEBUG" {
+		logrus.SetLevel(logrus.DebugLevel)
+	} else {
+		logrus.SetLevel(logrus.InfoLevel)
+	}
+}
+
+func setupRoutes() {
+	http.Handle("/docs/", http.StripPrefix("/docs/", httpSwagger.Handler(
+		httpSwagger.URL("http://127.0.0.1:8082/docs/swagger.json"),
+	)))
+
+	http.HandleFunc("/", pageHandler)
+}
+
 func fileExists(filename string) bool {
 	info, err := os.Stat(filename)
-	if os.IsNotExist(err) {
-		return false
-	}
-	return !info.IsDir()
+	return err == nil && !info.IsDir()
 }
 
-func PageHandler(w http.ResponseWriter, r *http.Request) {
-	// Extract the path component and strip the leading slash
+func pageHandler(w http.ResponseWriter, r *http.Request) {
 	path := strings.TrimPrefix(r.URL.Path, "/")
-	log.WithFields(log.Fields{
-		"path": path,
-	}).Debug("PageHandler invoked")
+	logrus.WithField("path", path).Debug("Handling page request")
 
-	// Delegate to Generic APIHandler for paths starting with 'api-handler'
 	if strings.HasPrefix(path, "api-handler") {
-		log.Debug("Path starts with 'api-handler', delegating to Generic APIHandler")
-		GenericAPIHandler(w, r)
+		genericAPIHandler(w, r)
 		return
 	}
 
-	// Delegate to APIHandler for paths starting with 'api'
 	if strings.HasPrefix(path, "api") {
-		log.Debug("Path starts with 'api', delegating to APIHandler")
-		APIHandler(w, r)
+		apiHandler(w, r)
 		return
 	}
 
-	//if path is blank, redirect to login
 	if path == "" {
 		path = "login"
 	}
 
-	// Construct the template file name
 	tmplFile := fmt.Sprintf("html/%s.html", path)
-
-	// Check if the template file exists
 	if !fileExists(tmplFile) {
-		log.WithFields(log.Fields{
-			"file": tmplFile,
-		}).Warn("Template file not found")
-
-		// Use http.NotFound to send a 404 response
+		logrus.WithField("file", tmplFile).Warn("Template not found")
 		http.NotFound(w, r)
 		return
 	}
 
-	// Construct the template
 	t, err := template.ParseFiles(tmplFile, "html/header.html", "js/login.js", "js/scripts.html")
 	if err != nil {
-		log.WithFields(log.Fields{
-			"error": err,
-			"file":  tmplFile,
-		}).Error("Error parsing template files")
+		logrus.WithError(err).WithField("file", tmplFile).Error("Template parsing error")
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
@@ -121,91 +90,47 @@ func PageHandler(w http.ResponseWriter, r *http.Request) {
 	if path != "login" {
 		page.Permission = api.Auth(w, r)
 	}
-	page.Title = strings.Title(path)          // Capitalize the first letter of the title
-	page.Layout = r.URL.Query().Get("layout") // Get the layout from the URL
-	log.Debug("Layout:", page.Layout)
-
-	log.WithFields(log.Fields{
-		"title":   page.Title,
-		"message": page.Message,
-	}).Debug("Executing template with page data")
+	page.Title = strings.Title(path)
+	page.Layout = r.URL.Query().Get("layout")
 
 	if err := t.Execute(w, page); err != nil {
-		log.WithFields(log.Fields{
-			"error": err,
-		}).Error("Error executing template")
+		logrus.WithError(err).Error("Template execution error")
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 }
 
-func APIHandler(w http.ResponseWriter, r *http.Request) {
-	log.Debug("APIHandler called")
-
-	// Extracting and logging the path component
+func apiHandler(w http.ResponseWriter, r *http.Request) {
 	path := strings.TrimPrefix(r.URL.Path, "/api/")
-	log.WithFields(log.Fields{
-		"Path": path,
-	}).Debug("API path identified")
+	logrus.WithField("path", path).Debug("API request received")
 
-	// API Key validation
-	apiKey := r.Header.Get("X-API-Key") // You can also use r.URL.Query().Get("api_key") if the key is in query params
+	apiKey := r.Header.Get("X-API-Key")
 	if !validateAPIKey(apiKey) && path != "signin" {
-		log.WithFields(log.Fields{
-			"API Key": apiKey,
-		}).Warn("Invalid or missing API Key")
+		logrus.WithField("apiKey", apiKey).Warn("Invalid API Key")
 		http.Error(w, "Unauthorized: Invalid API Key", http.StatusUnauthorized)
 		return
 	}
 
-	// Determining the path and calling the corresponding function
-	switch path {
-	case "signin":
-		log.Debug("Calling Signin function")
-		api.Signin(w, r)
-	case "logout":
-		log.Debug("Calling Signin function")
-		api.Logout(w, r)
-	case "products":
-		log.Debug("Calling ProductList function")
-		api.ProductList(w, r)
-	case "manufacturers":
-		log.Debug("Calling ListManufacturers function")
-		api.ListManufacturers(w, r)
-	case "productinsert":
-		log.Debug("Calling InsertProduct function")
-		api.InsertProduct(w, r)
-	case "productdelete":
-		log.Debug("Calling DeleteProduct function")
-		api.DeleteProduct(w, r)
-	case "productupdate":
-		log.Debug("Calling UpdateProduct function")
-		api.UpdateProduct(w, r)
-	case "users":
-		log.Debug("Calling ListUsersAPI function")
-		api.ListUsersAPI(w, r)
-	case "userupdate":
-		log.Debug("Calling UserDeleteAPI function")
-		api.UserUpdateAPI(w, r)
-	case "customers":
-		log.Debug("Calling ListCustomersAPI function")
-		api.ListCustomersAPI(w, r)
-	case "sortinglist":
-		log.Debug("Calling ListSortingsAPI function")
-		api.ListSortRequestsAPI(w, r)
-	case "sortingupdate":
-		log.Debug("Calling UpdateSortRequestAPI function")
-		api.UpdateSortRequestAPI(w, r)
-	case "sorterrorupdate":
-		log.Debug("Calling sortErrorUpdate function")
-		sql.SortErrorUpdate(w, r)
-	case "export":
-		log.Debug("Calling export function")
-		api.ExportDataAPI(w, r)
-	// Add other cases as needed
-	default:
-		log.WithFields(log.Fields{
-			"Path": path,
-		}).Warn("API path not found, returning 404")
+	apiRoutes := map[string]func(http.ResponseWriter, *http.Request){
+		"signin":          api.Signin,
+		"logout":          api.Logout,
+		"products":        api.ProductList,
+		"manufacturers":   api.ListManufacturers,
+		"productinsert":   api.InsertProduct,
+		"productdelete":   api.DeleteProduct,
+		"productupdate":   api.UpdateProduct,
+		"users":           api.ListUsersAPI,
+		"userupdate":      api.UserUpdateAPI,
+		"customers":       api.ListCustomersAPI,
+		"sortinglist":     api.ListSortRequestsAPI,
+		"sortingupdate":   api.UpdateSortRequestAPI,
+		"sorterrorupdate": sql.SortErrorUpdate,
+		"export":          api.ExportDataAPI,
+	}
+
+	if handler, exists := apiRoutes[path]; exists {
+		handler(w, r)
+	} else {
+		logrus.WithField("path", path).Warn("API endpoint not found")
 		http.Error(w, "Not Found", http.StatusNotFound)
 	}
 }
@@ -213,76 +138,48 @@ func APIHandler(w http.ResponseWriter, r *http.Request) {
 func validateAPIKey(key string) bool {
 	var exists bool
 	query := "SELECT EXISTS(SELECT 1 FROM purchasing.apikeys WHERE apikey = ?)"
-
-	// Query the database to check if the key exists
 	err := config.DB.QueryRow(query, key).Scan(&exists)
 	if err != nil {
-		log.WithFields(log.Fields{"error": err}).Error("Failed to query database for API key")
+		logrus.WithError(err).Error("Database query error")
 		return false
 	}
-
 	return exists
 }
 
-func GenericAPIHandler(w http.ResponseWriter, r *http.Request) {
-	log.Debug("GenericAPIHandler called")
-
-	// Extract the entire query string
-	queryString := r.URL.RawQuery
-
-	// Validate user session or permissions
-	if api.Auth(w, r).Role == "Unauthorized" {
-		log.Warn("Unauthorized access attempt to GenericAPIHandler")
-		http.Error(w, "Unauthorized", http.StatusUnauthorized)
-		return
-	}
-
-	// Extract target API endpoint
+func genericAPIHandler(w http.ResponseWriter, r *http.Request) {
+	logrus.Debug("Handling generic API request")
 	targetAPI := r.URL.Query().Get("targetAPI")
 	if targetAPI == "" {
-		log.Warn("Bad Request: Missing targetAPI parameter")
-		http.Error(w, "Bad Request: targetAPI parameter is required", http.StatusBadRequest)
+		logrus.Warn("Missing targetAPI parameter")
+		http.Error(w, "Bad Request: targetAPI parameter required", http.StatusBadRequest)
 		return
 	}
 
-	// Construct the full URL if targetAPI is a relative path
-	// Assuming you have a base URL for the API
-	baseURL := "http://127.0.0.1:8082" // Replace with the actual base URL
+	baseURL := "http://127.0.0.1:8082"
 	fullURL := baseURL + targetAPI
-	if queryString != "" {
-		fullURL += "?" + queryString
+	if r.URL.RawQuery != "" {
+		fullURL += "?" + r.URL.RawQuery
 	}
-	log.Debug("API Url being accessed: ", fullURL)
 
-	// Retrieve the API Key for the target API
-	// The API key is stored in an environment variable
 	apiKey := os.Getenv("APP_API_KEY")
-	log.WithFields(log.Fields{
-		"API-KEY": apiKey,
-	}).Debug("API Key")
 	if apiKey == "" {
-		log.Error("API Key for the target API is not set")
+		logrus.Error("API Key not set")
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
-	// Forwarding the request
 	proxyReq, err := http.NewRequest(r.Method, fullURL, r.Body)
 	if err != nil {
-		log.WithFields(log.Fields{"error": err}).Error("Failed to create new request for proxy")
+		logrus.WithError(err).Error("Request forwarding error")
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	// Copy headers from the original request to the proxy request
+	proxyReq.Header.Set("X-API-Key", apiKey)
 	copyHeader(r.Header, proxyReq.Header)
 
-	// Set the API key in the request header
-	proxyReq.Header.Set("X-API-Key", apiKey)
-
-	client := &http.Client{}
-	resp, err := client.Do(proxyReq)
+	resp, err := http.DefaultClient.Do(proxyReq)
 	if err != nil {
-		log.WithFields(log.Fields{"error": err}).Error("Failed to forward request to target API")
+		logrus.WithError(err).Error("API forwarding failed")
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
@@ -290,10 +187,7 @@ func GenericAPIHandler(w http.ResponseWriter, r *http.Request) {
 
 	copyHeader(resp.Header, w.Header())
 	w.WriteHeader(resp.StatusCode)
-	_, err = io.Copy(w, resp.Body)
-	if err != nil {
-		log.WithFields(log.Fields{"error": err}).Error("Failed to copy response body to client")
-	}
+	io.Copy(w, resp.Body)
 }
 
 func copyHeader(src, dest http.Header) {
